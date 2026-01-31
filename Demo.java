d738695d1b734e2951caad1026cd954752617da0
Thasmiya <galithasmiya@gmail.com>
2026-01-31 14:23:10 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,10 @@
+public class Demo {
+    public static void main(String args[]){
+    int a=5;
+    System.out.println("hello");
+    System.out.println("hello");
+    System.out.print(a);
+    System.out.println(a);
+    System.out.println(a);
+ }   
+}
