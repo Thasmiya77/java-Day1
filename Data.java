67a85fccb4f5224da1dc37add6f556fbede81a0a
Thasmiya <galithasmiya@gmail.com>
2026-01-31 15:51:32 +0530

    Add files via upload

---
+++ Data.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Data {
+    public static void main(String args[] ) {
+    Scanner obj=new Scanner(System.in);
+    int age=obj.nextInt();
+    float b=obj.nextFloat();
+    double c=obj.nextDouble();
+    String str1=obj.next();
+    String str2=obj.nextLine();
+    System.out.println(age);
+     System.out.println(b);
+      System.out.println(c);
+       System.out.println(str1);
+       System.out.println(str2); 
+    }
+}
